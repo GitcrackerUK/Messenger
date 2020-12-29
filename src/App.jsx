c16e24c15646141f54662094b3279d63719384e5
gitcrackerUK <pfsiwek@gmail.com>
2020-12-29 16:36:10 +0000

    GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Main from 'components/layout/Main';
+import {createGlobalStyle} from 'styled-components';
+ 
+const GlobalStyles = createGlobalStyle`
+body{
+background-color:#E5E5E5;
+display:flex;
+align-items:center;
+justify-content:center;
+padding:0px;
+margin:0px;
+min-height:1000px;
+}
+`
+
 
 function App() {
     return (
         <div>
-            <h1>hello world</h1>
+            <GlobalStyles></GlobalStyles>
+            <Main></Main>
         </div>
     )
 }
