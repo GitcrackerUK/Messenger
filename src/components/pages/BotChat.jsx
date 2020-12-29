834922b6f15a1eb0f73daec72de21f366cdd385c
gitcrackerUK <pfsiwek@gmail.com>
2020-12-29 20:26:25 +0000

    robo img

--- src/components/pages/BotChat.jsx
+++ src/components/pages/BotChat.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import bot from 'components/img/bot.jpg'
- // import styled from 'styled-components'
+import bot from 'img/bot.jpg'
+ import styled from 'styled-components'
 import MainLayout from 'components/layout/Main'
 import Text from 'components/common/text'
 
+const Robo = styled.img`
+display:flex;
+height:120px;
+width:120px;
+border-radius:5px;
+margin:auto;
+`
 
 export default function BotChat() {
     return (
         <MainLayout header="Bot Chat" left="Chat to Bot" right="Chat History" leftLink="/start" rightLink="history">
           <Text>Chat Bot is simple app allowing any one to  have conversation with AI. It’s simple and fun. Choose you avatar and type your name.</Text>
-          <img src={bot} alt="bot-img"></img>
+          <Robo src={bot} alt="bot-img"></Robo>
         </MainLayout>
     )
 }
