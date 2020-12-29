95cfaa6b2d6f1ee1921232ffd013f225023f6c81
gitcrackerUK <pfsiwek@gmail.com>
2020-12-29 18:04:00 +0000

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BotChat from 'components/pages/BotChat';
 import Chat from 'components/pages/Chat';
 import ChatHistory from 'components/pages/ChatHistory';
 import SingleHistory from 'components/pages/SingleHistory';
 import StartChat from 'components/pages/StartChat';
-import {createGlobalStyle} from 'styled-components';
- 
+import { createGlobalStyle } from 'styled-components';
+
 const GlobalStyles = createGlobalStyle`
 body{
 background-color:#E5E5E5;
@@ -23,11 +24,30 @@ function App() {
     return (
         <div>
             <GlobalStyles></GlobalStyles>
-            <BotChat></BotChat>
-            <StartChat></StartChat>
-            <Chat></Chat>
-            <ChatHistory></ChatHistory>
-            <SingleHistory></SingleHistory>
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <BotChat></BotChat>
+                    </Route>
+                    <Route path="start">
+                        <StartChat></StartChat>
+                    </Route>
+                    <Route path="chat">
+                        <Chat></Chat>
+                    </Route>
+
+                    <Route path="history">
+                        <ChatHistory></ChatHistory>
+                    </Route>
+
+                    <Route path="single-history">
+                        <SingleHistory></SingleHistory>
+                    </Route>
+
+                </Switch>
+
+            </Router>
+
         </div>
     )
 }
