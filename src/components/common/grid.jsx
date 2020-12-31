65492a46eaac4543c32ea0cd13b439ed50e57a3b
gitcrackerUK <pfsiwek@gmail.com>
2020-12-31 12:34:48 +0000

    imported users imgs to grid

--- src/components/common/grid.jsx
+++ src/components/common/grid.jsx
@@ -1,8 +1,7 @@
-import { user, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11 } from 'img/index';import React , { useState } from 'react'
-import styled from 'styled-components'
-
-
-
+import { user, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11 } from 'img/index';
+import React , { useState } from 'react';
+import styled from 'styled-components';
+import StImg from './Thumbnail';
 
 
 const Wrapper = styled.div`
@@ -12,20 +11,15 @@ height:147px;
 display:flex;
 flex-wrap:wrap;
 `
-const StImg = styled.img`
-height:45px;
-width:45px;
-margin:5px;
-border-radius:5px;
-`
+
 
 export default function Grid() {
     const [users] = useState([user, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11])
+
     return (
         <Wrapper>
             {
                 users.map((p) => {
-                    console.log(p)
                     return(
                         <StImg src={p} id={p.substring(p.length-10)} >
                         </StImg>
