328856d422fe352c6ce68639fceec12f22438573
gitcrackerUK <pfsiwek@gmail.com>
2020-12-31 12:33:16 +0000

    button

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+margin:auto;
+background-color:rgba(196, 196, 196, 0.0);
+text-transform:uppercase;
+color:#01579B;
+font-size:8.5px;
+width:54px;
+height:22px;
+border:solid 1px rgba(0, 0, 0, 0.12);
+border-radius:3px;
+outline: none;
+&:hover{
+    background-color:#01579B;
+    color:#fff
+}
+`
+
+export default Button
