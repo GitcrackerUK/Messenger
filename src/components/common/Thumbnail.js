54c0ae4eba47c0ceaa474e49d2b5d058ed714ddc
gitcrackerUK <pfsiwek@gmail.com>
2020-12-31 12:35:27 +0000

    text and thumbnails for chat history

---
+++ src/components/common/Thumbnail.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StImg = styled.img`
+margin:auto;
+height:45px;
+width:45px;
+margin:5px;
+border-radius:5px;
+`
+
+export default StImg
---
+++ src/components/common/historyText.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Text = styled.p`
+margin:auto;
+font-size:14px;
+color:rgba(0, 0, 0, 0.7);
+text-align:center;
+`
+export default Text
