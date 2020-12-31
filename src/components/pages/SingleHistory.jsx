df586c1406eb2b4258e8bf751cf060fed344dfe9
gitcrackerUK <pfsiwek@gmail.com>
2020-12-31 13:34:04 +0000

    imported cht window and messages

--- src/components/pages/SingleHistory.jsx
+++ src/components/pages/SingleHistory.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-// import styled from 'styled-components'
+import BlueMessage from 'components/common/BlueResponse'
+import WhiteMessage from 'components/common/WhiteMessage'
+import ChatWindow from 'components/common/chatWrapper'
 import MainLayout from 'components/layout/Main'
 
 
 export default function BotChat() {
     return (
         <MainLayout header="Chat History" left="Chat to Bot" right="Go Back" leftLink="/start" rightLink="/history">
-          
+          <ChatWindow>
+              <WhiteMessage>Hi</WhiteMessage>
+              <BlueMessage>Hello</BlueMessage>
+              <WhiteMessage>How are you??</WhiteMessage>
+
+
+          </ChatWindow>
         </MainLayout>
     )
 }
