47cb7d53334f1b855f3e84589727ca1655f11128
gitcrackerUK <pfsiwek@gmail.com>
2021-01-03 22:24:21 +0000

    useState for name id and name.lenght validation

--- Client/src/components/pages/StartChat.jsx
+++ Client/src/components/pages/StartChat.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from 'components/common/input'
 import MainLayout from 'components/layout/Main'
 import styled from 'styled-components'
 import Grid from 'components/common/grid'
 import NavButton from 'components/common/Nav-item'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 const InputWrapper = styled.div`
@@ -14,24 +14,38 @@ const ButtonWrapper = styled.div`
 margin-top:25px;
 `
 const StLink = styled(Link)`
-/* margin:auto; */
 text-decoration:none;
 color:rgba(0, 0, 0, 0.7);
 font-size:13px;
 `
 
 export default function StartChat() {
-    // const { images, setImages} = useState([])
+    const [name,setName] = useState({name:'',minLength:false})
+    const [user, setUser] = useState({ id: '', name: '' })
+    function handleClick(e) {
+        setUser(prev=>{return{...prev,id: e}})
+    }
+    function updateName(e) {
+        setUser(prev=>{return{...prev,name:e.target.value}})
+        setName({name:e.target.value})
+        if(name.name.length>2){
+            setName(p=>({...p,minLength:true}))
+        }else{
+            setName(p=>({...p,minLength:false}))
+        }
+    }
 
+    //change color theme for input
+    //grid
+    //StLink
     return (
         <MainLayout header="Start Chat" left="Chat History" leftLink="/history" rightLink="/" right="Go Back" >
-            <InputWrapper>
-                <Input label="Name" underText="Type your name" placeholder="Input text"></Input>
+            <InputWrapper  onChange={(e)=>updateName(e)}>
+                <Input label="Name"  underText="Type your name" placeholder="Input text"></Input>
             </InputWrapper>
-            <Grid></Grid>
-
-            <ButtonWrapper>
-               <StLink to='/chat'><NavButton>Start Chat</NavButton></StLink>
+            <Grid handleClick={e => handleClick(e)} ></Grid>
+            <ButtonWrapper >
+                <StLink onCLick={updateName} to={name.minLength?{ pathname: '/chat', state: { user } }:'/start'}><NavButton>Start Chat</NavButton></StLink>
             </ButtonWrapper>
 
 
