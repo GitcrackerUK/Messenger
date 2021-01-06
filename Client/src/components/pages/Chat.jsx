25688b7afcb2fdfd3d15f1f567239e905f0d6017
gitcrackerUK <pfsiwek@gmail.com>
2021-01-06 23:16:22 +0000

    time stamp for setChat

--- Client/src/components/pages/Chat.jsx
+++ Client/src/components/pages/Chat.jsx
@@ -1,25 +1,29 @@
-import axios from 'axios';
-import React, { useState,useEffect } from 'react'
-import {useLocation} from 'react-router-dom'
-import MainLayout from 'components/layout/Main'
-import Input from 'components/common/input'
-import BlueMessage from 'components/common/BlueResponse'
 import WhiteMessage from 'components/common/WhiteMessage'
+import BlueMessage from 'components/common/BlueResponse'
 import ChatWindow from 'components/common/chatWrapper'
+import React, { useState,useEffect } from 'react'
+import MainLayout from 'components/layout/Main'
+import {useLocation} from 'react-router-dom'
+import Input from 'components/common/input'
+import axios from 'axios';
 const parseString = require('xml2js').parseString;
 
 
+
 export default function Chat() {
-    useEffect(() => {
-        console.log(data);
-    }, )
-    
+    const [User, setUser] = useState({})  
     const [formInput, setInput] = useState('');
     const [chat, setChat] = useState([])
     const data = useLocation() // value of name and id from start component
 
+    useEffect(() => {
+        setUser(prev=>{return{...prev,user:data.state.user}})
+        setUser(prev=>{return{...prev,userChat:chat}})
+    },[])
 
-   
+   function Time(){
+       return new Date().toLocaleTimeString();
+   }
     function toBottom(){
         const wrapper = document.getElementById("wrapper");
         if(wrapper.scrollHeight!==null){return wrapper.scrollTop = wrapper.scrollHeight - wrapper.clientHeight}
@@ -31,7 +35,8 @@ export default function Chat() {
         setChat(prevState =>{
             return ([...prevState,{
                 user: true,
-                text: formInput
+                text: formInput,
+                time: Time()
             }])
         })
         axios.get(`https://www.botlibre.com/rest/api/form-chat?&application=4120366723131427615&instance=165&message=${formInput}`)
@@ -40,7 +45,8 @@ export default function Chat() {
                       setChat(prevState =>{
                         return [...prevState,{
                             user: false,
-                            text: result.response.message[0]
+                            text: result.response.message[0],
+                            time: Time()
                         }]
                     })
                 });
@@ -55,8 +61,6 @@ export default function Chat() {
     function handleChange(e) {
         setInput(e.target.value)
     }
-
-
 
     const inputField = <form id="field" onSubmit={(e) => {Submit(e)}} onChange={(e) => handleChange(e)}>
         <Input label="Chat"  placeholder="Input text" underText="Type in whatever you like"  ></Input>
