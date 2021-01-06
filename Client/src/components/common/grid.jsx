d2993470a2661d62a6312c53fdd3475f6c570e5a
gitcrackerUK <pfsiwek@gmail.com>
2021-01-06 18:43:32 +0000

    import Cel

--- Client/src/components/common/grid.jsx
+++ Client/src/components/common/grid.jsx
@@ -1,7 +1,7 @@
 import { user, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11 } from 'img';
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import StImg from './Thumbnail';
+import Cel from './Cel'
 
 const Wrapper = styled.div`
 margin:20px auto;
@@ -11,21 +11,27 @@ display:flex;
 flex-wrap:wrap;
 `
 
+
 export default function Grid(props) {
     const [users] = useState([user, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11])
+    const [tick, setTick]= useState(false)
 
+    function handleTick(){
+        
+    }
     return (
         <Wrapper>
             {
                 users.map((p) => {
                     return (
-                        <StImg onClick={e => props.handleClick(e.target.id)} src={p} id={p.substring(p.length - 10)} key={p.substring(p.length - 10)}>
-                        </StImg>
+                        <Cel  tick={true} onClick={e => props.handleClick(e.target.id,)}  src={p} id={p.substring(p.length - 10)} key={p.substring(p.length - 10)}></Cel>
+                        
                     )
 
                 })
 
             }
+           
         </Wrapper>
     )
 
